7f055c7041f074b060b211f816dc4c3bf0aa0238
Jesse Schneider <jessewschneider96@gmail.com>
2019-08-04 09:28:18 +1000

    straightened out merge, prototype runs correctly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
-  { path: 'post-details', loadChildren: './post-details/post-details.module#PostDetailsPageModule' },
-  { path: 'event-details', loadChildren: './event-details/event-details.module#EventDetailsPageModule' },
-  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' }
+ 
 
 const routes: Routes = [
   {
@@ -20,7 +11,8 @@ const routes: Routes = [
   { path: 'post-details', loadChildren: './post-details/post-details.module#PostDetailsPageModule' },
   { path: 'event-details', loadChildren: './event-details/event-details.module#EventDetailsPageModule' },
   { path: 'event-creation', loadChildren: './event-creation/event-creation.module#EventCreationPageModule' },
-  { path: 'employment-post-creation', loadChildren: './employment-post-creation/employment-post-creation.module#EmploymentPostCreationPageModule' }
+  { path: 'employment-post-creation', loadChildren: './employment-post-creation/employment-post-creation.module#EmploymentPostCreationPageModule' },
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' }
 ];
 @NgModule({
   imports: [
