a8abcf3c0a2922d7a1d20d9de035795890fed6af
Jesse Schneider <jessewschneider96@gmail.com>
2019-08-03 20:05:44 +1000

    added styling to job/internship advetisements

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -15,21 +15,20 @@ export class Tab4Page implements OnInit {
 
   }
   jobs = [
-    { title: "Full-stack Engineer", hirer: "Microsoft", description: "New Position"},
+    { title: "Fullstack Engineer", hirer: "Microsoft", description: "The Operations Applications Group within the Cloud + AI Business Applications Group specializes in best in class solutions enabling businesses to run their end to end operations for the Dynamics 365 line of Enterprise Resource and Planning (ERP) in a Mobile-First/Cloud-First world. Our goal is to enable customers to effectively run their business using the Dynamics 365 products, delivering complete solutions that target the increasing demands of midsize and larger companies. Whether deployed to the cloud or on premise, Dynamics 365 integrates several Microsoft technologies to create a large-scale application. Our culture is inclusive, casual and high energy; our team members come from diverse backgrounds and grounded in our customer needs. Our team is accountable and provides its members a variety of opportunities to learn and grow their careers.",
+responsibilities: ["Design, develop, test & support features, experiences & solutions for end-to-end business scenarios over the full stack using modern development technologies", "Collaborate across diverse engineering teams to solve challenging problems for large-scale business SaaS applications", "Accountability for the quality, usability & performance of features", "Debug and handle customer reported issues"] },
     { title: "Testing Engineer", hirer: "Google", description: "New Position"},
-    { title: "Front-end Developer", hirer: "AWS", description: "New Position"},
-    { title: "Back-end Developer", hirer: "Redhat", description: "New Position"},
+    { title: "Frontend Developer", hirer: "AWS", description: "New Position"},
+    { title: "Backend Developer", hirer: "Redhat", description: "New Position"},
     { title: "Systems Engineer", hirer: "Alatassian", description: "New Position"},
 ];
+
+
 
 selectedJob = {};
 
 async presentModal(i: number) {
-  console.log(i);
   this.selectedJob = this.jobs[i];
-  console.log(this.selectedJob);
-
-
   const modal = await this.modalController.create({
     component: JobModalPage,
     componentProps: {
