6f458d2f78bf8c0648f3b4c41c6d968b6acc0cf6
thefleshthathates99 <mytab99@gmail.com>
2019-08-03 12:19:05 +1000

    additional commit
    Added dummy data to of newsFeed

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,5 +9,26 @@ export class Tab2Page {
 
   constructor() {}
 
-  
+  newsData = [
+    {
+        eventTitle: "Study Sesh",
+        userPosted: "Derrick McGowan",
+        details: "event details data here",
+    },
+    {
+      eventTitle: "Alumni Soiree",
+      userPosted: "Allan McCuen",
+      details: "event details data here",
+  },
+  {
+      eventTitle: "Looking for Tutour",
+      userPosted: "Geoffery CuddleTrousers",
+      details: "I am bad at study, plz halp",
+  },
+  {
+      eventTitle: "Hackathon",
+      userPosted: "Yogg'Sathtoth",
+      details: "Looking for a team for the Hackathon",
+  }
+  ];
 }
