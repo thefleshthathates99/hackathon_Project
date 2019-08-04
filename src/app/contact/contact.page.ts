4fcc2653571206d791e22efc6e1dd43b95b31561
thefleshthathates99 <mytab99@gmail.com>
2019-08-04 12:18:36 +1000

    completed contacts page

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -12,4 +12,11 @@ export class ContactPage implements OnInit {
   ngOnInit() {
   }
 
+  contactees = [
+    {name: "Jerrad Butler", position: "Employment Officer - Microsoft", status: "Online - Message Anytime"},
+    {name: "McKayla Nickleberg", position: "CEO - Ironbark Software", status: "Offline"},
+    {name: "Derrek", position: "Griffith Alumni", status: "Inactive"},
+    {name: "Allan McGrewin", position: "Level 3/4 Service Desk - 3 years experience", status: "Private"},
+    {name: "Alexa Applebaum", position: "HRM Manager - Griffith Lecturer", status: "Online - Message Anytime"}
+  ]
 }
